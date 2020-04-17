d7483098e8f543498c75dfe9b1454a32efd19fae
Sonali <sonaligarg234@gmail.com>
2020-04-17 18:19:18 +0530

    remove

--- newsutility.py
+++ newsutility.py
@@ -1,11 +1,11 @@
 from newspaper import *
+import webbrowser
 url="https://www.bbc.com/news/uk-52304914"
-cnn_article=Article(url,language="en")
+cnn_article=Article(url,languages="en")
 print(cnn_article.url)
 cnn_article.download()
 cnn_article.parse()
 cnn_article.nlp()
-print(cnn_article.text)
-hs=open("2.html",'w')
+hs=open("1.html",'w')
 hs.write(cnn_article.text)
 hs.close()
